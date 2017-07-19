d245ced84b4a85f4b8a756e49e88128f1b3161f1
vedhanayaki <vedhanayaki1996@gmail.com>
2017-07-19 02:22:37 -0700

    Add files via upload

---
+++ BinarytoDecimal.java
@@ -0,0 +1,15 @@
+package player;
+import java.util.Scanner;
+public class BinarytoDecimal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Enter the Number");
+		Scanner s=new Scanner(System.in);
+		int a=12;
+		String strBinaryNumber=Integer.toBinaryString(a);
+		System.out.println(strBinaryNumber);
+	}
+
+}
